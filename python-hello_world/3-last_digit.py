cae029a7629919959459bbe149d4129d73b7b787
Amychy <amychyihejiako@gmail.com>
2023-07-24 19:33:49 +0100

    changes in the 3 last digit file again

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
-if number < 0:
-    last_digit *= -1
-    print("last digit of", number, "is", last_digit, end=" ")
-if last_digit == 0:
-    print("and is 0")
-elif last_digit > 5:
-    print("and is greater than 5")
-else:
-    print("and is less than 6 and not 0")
-    
+num = number - (10 * int(number / 10))
+# YOUR CODE HERE
+if num > 5:
+    print('Last digit of {} is {} and is greater than 5'.format(number,num), end='\n')
+elif num == 0:
+    print('Last digit of {} is {} and is 0'.format(number,num), end='\n')
+elif num < 6 != 0:
+    print('Last digit of {} is {} and is less than 6 and not 0'.format(number,num),end='\n')
