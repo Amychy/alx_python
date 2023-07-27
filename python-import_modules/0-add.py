d6bf99a98ce96f62b01d75fd4a7073a2b0014e6c
Amychy <amychyihejiako@gmail.com>
2023-07-27 14:06:15 +0100

    started the import module tasks

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,8 +3,10 @@ from add_0 import add
 def main():
     a = 1
     b = 2
+    from add_0 import add
+
     result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    print("{} + {} = {}".format(a,b,result)
 
 if __name__ == "__main__":
     main()
