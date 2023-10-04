9cb947abb639c2a72b8d6636c1899512a7313a4e
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-04 21:24:53 +0100

    Update 3-safe_print_division.py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a / b
+        quot = a / b
     except ZeroDivisionError:
-        result = None
+        quot = None
     finally:
-        print("Inside result: {}".format(result))
-        if result is not None:
-            print("{} / {} = {}".format(a, b, result))
-
-# Test the function
-a = 10
-b = 2
-safe_print_division(a, b)
-
-c = 5
-d = 0
-safe_print_division(c, d)
+        print("Inside result: {}".format(quot))
+    return quot
