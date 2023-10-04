6462c7ab650378694014de050bcbf53fbca9d52f
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-05 00:32:33 +0100

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-""" defines a square based on size  """
-
+"""defines class square"""
 
 class Square:
-    """ square with private instance attribute size """
+    """square with private instance attribute size"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+my_square = Square(3)
