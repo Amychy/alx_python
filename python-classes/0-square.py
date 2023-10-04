b5ebf1b2fa5cfe7b2b45c2d2c577ba2124423c0d
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-05 00:05:13 +0100

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
+"""defines class square """
+
+
 class Square:
-    def __init__(self, size):
-        self.__size = size
-
-    def area(self):
-        return self.__size ** 2
-
-    def get_size(self):
-        return self.__size
-
-    def set_size(self, size):
-        self.__size = size
+    """ empty square """
+    pass
