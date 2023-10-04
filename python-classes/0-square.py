d7e6ae56a46f0567f6da6a3b7f40c385ffb22158
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-05 00:22:29 +0100

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """defines class square """
 
-
 class Square:
     """ square with size attribute """
     def __init__(self, size):
         self.__size = size
 
 my_square = Square(3)
+print(type(my_square))
 print(my_square.__dict__)
