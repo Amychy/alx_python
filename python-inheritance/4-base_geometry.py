67b6e8cc0ba028b48cddd0a0955c488f17f486a0
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-05 02:53:48 +0100

    Update 4-base_geometry.py

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""
-Contains the class BaseGeometry
-"""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """A class with public attribute area"""
+    """this class represents a base geometry"""
+
+    def area(self):
+        """method not implemented yet"""
+        raise Exception("area() is not implemented")
