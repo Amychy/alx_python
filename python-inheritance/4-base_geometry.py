ab678a3b4c0216d90a05b7355a079bcc13eb70ff
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-05 03:13:02 +0100

    Update 4-base_geometry.py

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -6,8 +6,8 @@ class BaseGeometry:
     """this class represents a base geometry"""
 
     def __dir__(self):
-        """Includes the default attributes and methods and the 'area' method without duplicates."""
-        return list(set(dir(type(self))) | {'area'})
+        """Includes only the desired attributes and methods."""
+        return ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'area']
 
     def area(self):
         """method not implemented yet"""
