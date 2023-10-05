de88a0eb2557ed856c439eeeaa84cf65faa4f5e2
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-05 03:05:59 +0100

    Update 4-base_geometry.py

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -5,6 +5,10 @@
 class BaseGeometry:
     """this class represents a base geometry"""
 
+    def __dir__(self):
+        """Excludes the 'area' method from the list of attributes."""
+        return [attribute for attribute in super().__dir__() if attribute != 'area']
+
     def area(self):
         """method not implemented yet"""
         raise Exception("area() is not implemented")
