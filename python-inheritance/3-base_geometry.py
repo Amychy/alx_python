98d6c0a7c68983365d19342392ec3f6ef6bc3c0b
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-05 01:48:07 +0100

    Update 3-base_geometry.py

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,31 +1,6 @@
 #!/usr/bin/python3
 """ contains class BaseGeometry """
 
-
 class BaseGeometry:
     """ defines class BaseGeometry """
-    __class__ = None
-    __delattr__ = None
-    __dict__ = None
-    __dir__ = None
-    __doc__ = None
-    __eq__ = None
-    __format__ = None
-    __ge__ = None
-    __getattribute__ = None
-    __gt__ = None
-    __hash__ = None
-    __init__ = None
-    __le__ = None
-    __lt__ = None
-    __module__ = None
-    __ne__ = None
-    __new__ = None
-    __reduce__ = None
-    __reduce_ex__ = None
-    __repr__ = None
-    __setattr__ = None
-    __sizeof__ = None
-    __str__ = None
-    __subclasshook__ = None
-    __weakref__ = None
+    pass
