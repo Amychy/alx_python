971c1412cf03cc04b404bf9b786909929b6ba376
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-06 10:48:44 +0100

    Update 6-rectangle.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,28 +1,41 @@
 #!/usr/bin/python3
 """Defines a Rectangle class that inherits from BaseGeometry."""
 
-BaseGeometry = __import__('5-base_geometry').BaseGeometry
+class BaseGeometry:
+    """This class represents a base geometry."""
+
+    def area(self):
+        """Method not implemented yet"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter as an integer.
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
-    """Represents a rectangle."""
+    """This class represents a rectangle."""
 
     def __init__(self, width, height):
-        """Initializes a Rectangle instance."""
-        super().__init__()
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """
+        Initialize a Rectangle instance with a given width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
         self.__width = width
         self.__height = height
-    def __dir__(self):
-        """Includes only the desired attributes and methods."""
-        desired_attrs = [
-            '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__',
-            '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__',
-            '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__',
-            '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__',
-            'area', 'integer_validator'
-        ]
-        return [attr for attr in desired_attrs if attr != '__init_subclass__']
 
-    def __init_subclass__(cls):
-        pass
+        # Validate width and height as positive integers
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
