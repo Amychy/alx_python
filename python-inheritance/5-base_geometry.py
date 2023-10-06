4ea692caeecc46157e3e35db8f1aed3c94eed384
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-06 09:50:35 +0100

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -6,12 +6,33 @@ class BaseGeometry:
     """This class represents a base geometry."""
 
     def area(self):
-        """Raises an exception with the message 'area() is not implemented'."""
+        """Method not implemented yet"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates the 'value' argument."""
-        if not isinstance(value, int):
+        """Validate a parameter as an integer.
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+    def __dir__(self):
+        """Includes only the desired attributes and methods."""
+        desired_attrs = [
+            '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__',
+            '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__',
+            '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__',
+            '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__',
+            'area', 'integer_validator'
+        ]
+        return [attr for attr in desired_attrs if attr != '__init_subclass__']
+
+    def __init_subclass__(cls):
+        pass
