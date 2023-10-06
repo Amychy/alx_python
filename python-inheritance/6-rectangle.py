2a7f4da73340bf2e27487a8fe438edcbea3b6340
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-06 09:56:32 +0100

    Update 6-rectangle.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -13,3 +13,10 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+    def __dir__(self):
+        """Return a list of attributes and methods for Rectangle."""
+        return [
+            '__class__', '__delattr__', '__doc__', '__eq__', '__format__', '__ge__',
+            '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__',
+            '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__',
+            '__sizeof__', '__str__', '__subclasshook__', 'area', 'integer_validator']
