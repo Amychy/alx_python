6d9b9d768b0dbb3a3f23b892ec418734a9613b8b
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-06 13:14:57 +0100

    Update 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -4,15 +4,22 @@ Usage: ./2-post_email.py <URL> <email>
   - Displays the body of the response.
 """
 import sys
-import urllib.parse
-import urllib.request
-
+import requests
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    if len(sys.argv) != 3:
+        print("Usage: ./2-post_email.py <URL> <email>")
+        sys.exit(1)
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    payload = {"email": email}
+    
+    try:
+        response = requests.post(url, data=payload)
+        response.raise_for_status()  # Raise an exception for HTTP errors (4xx, 5xx)
+        print(response.text)
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+        sys.exit(1)
