56aad109732858678c6917f3135782502fdcd7f5
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-06 12:25:10 +0100

    Update 6-rectangle.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,53 +1,19 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class that inherits from BaseGeometry."""
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
-class BaseGeometry:
-    """This class represents a base geometry."""
 
-    def area(self):
-        """Method not implemented yet"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate a parameter as an integer.
-        Args:
-            name (str): The name of the parameter.
-            value (int): The parameter to validate.
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-class CustomDirMeta(type):
-    def __dir__(cls):
-        """Includes only the desired attributes and methods."""
-        desired_attrs = [
-            '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__',
-            '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__',
-            '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__',
-            '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__',
-            'area', 'integer_validator'
-        ]
-        return desired_attrs
-
-class Rectangle(BaseGeometry, metaclass=CustomDirMeta):
-    """This class represents a rectangle."""
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
 
     def __init__(self, width, height):
-        """
-        Initialize a Rectangle instance with a given width and height.
+        """Intialize a new Rectangle.
 
         Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
         """
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
-
-        # Validate width and height as positive integers
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
