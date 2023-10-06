b0cabaf2d98830114668ef174716c330a475b888
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-06 13:18:44 +0100

    Update 6-my_github.py

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 import sys
 import requests
-"""A script that:
-- takes your GitHub credentials (username and password)
-- uses the GitHub API to display your id
-"""
-import sys
-import requests
 from requests.auth import HTTPBasicAuth
-
 
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id")
+    user_info = r.json()
+    
+    if r.status_code == 200:
+        print(user_info.get("id"))
+    else:
+        print(None)
