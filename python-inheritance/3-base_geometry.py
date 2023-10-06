20f2d560674625d263b38689e208aaa9cfcd8e2f
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-06 10:32:08 +0100

    Update 3-base_geometry.py

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines a base geometry class BaseGeometry."""
 
-class ExcludeInitSubclass(type):
+class ExcludeInitSubclassMeta(type):
     def __dir__(cls):
         """Includes only the desired attributes and methods."""
         desired_attrs = [
@@ -12,7 +12,7 @@ class ExcludeInitSubclass(type):
         ]
         return [attr for attr in desired_attrs if attr != '__init_subclass__']
 
-class BaseGeometry(metaclass=ExcludeInitSubclass):
+class BaseGeometry(metaclass=ExcludeInitSubclassMeta):
     """
     This is the base geometry class.
     It is currently an empty class.
