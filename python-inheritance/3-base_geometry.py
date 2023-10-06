ee24273e2a5b5a5e3962c0244e28deab7978f598
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-06 09:02:00 +0100

    Update 3-base_geometry.py

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -3,12 +3,12 @@
 
 
 class BaseGeometry:
-    """this class represents a base geometry"""
+    """This class represents a base geometry."""
 
     def __dir__(self):
         """Includes only the desired attributes and methods."""
-        return ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__']
+        return [attr for attr in super().__dir__() if attr != '__init_subclass__']
 
     def area(self):
-        """method not implemented yet"""
+        """Method not implemented yet."""
         raise Exception("area() is not implemented")
