d1cbef3ce3661a7277641e23c3c6706d7ebc2e33
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-06 09:58:09 +0100

    Update 6-rectangle.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -14,9 +14,15 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
     def __dir__(self):
-        """Return a list of attributes and methods for Rectangle."""
-        return [
-            '__class__', '__delattr__', '__doc__', '__eq__', '__format__', '__ge__',
-            '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__',
-            '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__',
-            '__sizeof__', '__str__', '__subclasshook__', 'area', 'integer_validator']
+        """Includes only the desired attributes and methods."""
+        desired_attrs = [
+            '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__',
+            '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__',
+            '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__',
+            '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__',
+            'area', 'integer_validator'
+        ]
+        return [attr for attr in desired_attrs if attr != '__init_subclass__']
+
+    def __init_subclass__(cls):
+        pass
