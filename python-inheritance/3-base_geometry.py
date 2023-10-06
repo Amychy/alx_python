7209a06a935bb92cd51ad5d39f122bc641c218f1
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-06 09:21:30 +0100

    Update 3-base_geometry.py

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
 class BaseGeometry:
+    """This class represents a base geometry."""
+
     def __dir__(self):
         """Includes only the desired attributes and methods."""
         desired_attrs = [
