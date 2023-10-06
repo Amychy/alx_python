8e3d7fb708524ed5325541462ba0498d2574bc3d
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-06 17:22:49 +0100

    Update 6-number_odd_or_even.py

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -29,12 +29,12 @@ def display_number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_number_template(n):
-    return render_template('7-number.html', number=n)
+    return render_template('5-number.html', number=n)
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def display_number_odd_or_even(n):
     odd_or_even = "odd" if n % 2 != 0 else "even"
-    return render_template('7-number_odd_even.html', number=n, odd_even=odd_or_even)
+    return render_template('6-number_odd_even.html', number=n, odd_even=odd_or_even)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
