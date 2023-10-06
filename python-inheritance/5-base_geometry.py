29267d852a633d3450441486ce50c6737bcd247d
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-06 09:47:29 +0100

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry"""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """this class represents a base geometry"""
+    """This class represents a base geometry."""
 
     def area(self):
-        """method not implemented yet"""
+        """Raises an exception with the message 'area() is not implemented'."""
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the 'value' argument."""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
