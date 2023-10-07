56f167e7f6116d4da0438aeda33047bbf275d304
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-07 23:57:18 +0100

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -3,6 +3,15 @@ import requests
 
 # Function to fetch tasks for a given user ID
 def fetch_user_tasks(user_id):
+    """
+    Fetches tasks for a given user ID from the API.
+
+    Args:
+        user_id (int): The ID of the user for whom tasks should be fetched.
+
+    Returns:
+        list: A list of tasks for the user in JSON format.
+    """
     todos_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
     todos_response = requests.get(todos_url)
     if todos_response.status_code != 200:
@@ -11,6 +20,12 @@ def fetch_user_tasks(user_id):
 
 # Function to fetch all user IDs
 def fetch_user_ids():
+    """
+    Fetches a list of all user IDs from the API.
+
+    Returns:
+        list: A list of user IDs.
+    """
     users_url = "https://jsonplaceholder.typicode.com/users"
     users_response = requests.get(users_url)
     if users_response.status_code != 200:
@@ -19,6 +34,9 @@ def fetch_user_ids():
 
 # Function to export data in JSON format for all tasks from all employees
 def export_todo_all_employees():
+    """
+    Fetches tasks for all employees and exports the data in JSON format to a file.
+    """
     all_employee_data = {}
 
     user_ids = fetch_user_ids()
@@ -26,9 +44,8 @@ def export_todo_all_employees():
         user_tasks = fetch_user_tasks(user_id)
         employee_name = user_tasks[0]["username"] if user_tasks else "Unknown Employee"
         
-        #a list of tasks for the current user
+        # A list of tasks for the current user
         employee_tasks = [{"username": employee_name, "task": task["title"], "completed": task["completed"]} for task in user_tasks]
-        
         
         all_employee_data[user_id] = employee_tasks
 
