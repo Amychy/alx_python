f3c308d619aedbb43ffa645228c2c407bcc20c8c
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-07 22:03:24 +0100

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -18,19 +18,13 @@ def get_employee_todo_list_progress(employee_id):
     employee_data = employee_response.json()
     todo_data = todo_response.json()
 
-    # Calculate the number of completed and total tasks
-    total_tasks = len(todo_data)
-    completed_tasks = sum(1 for task in todo_data if task["completed"])
-
     # Print the employee's TODO list progress
-    print(f"Employee {employee_data['name']} is done with tasks({completed_tasks}/{total_tasks}):")
+    print(f"Employee {employee_data['name']} is done with tasks:")
 
-    # Print the titles of completed tasks
+    # Print the titles and status of all tasks
     for task in todo_data:
-        if task["completed"]:
-            print(f"Task {task['id']} Formatting: OK")
-        else:
-            print(f"Task {task['id']} Formatting: Incorrect")
+        task_status = "OK" if task["completed"] else "Incomplete"
+        print(f"Task {task['id']} in output: {task_status}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
