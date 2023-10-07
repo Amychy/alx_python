9c5d5221ec867d361dbf82c145ecdc7cac67221f
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-07 23:52:09 +0100

    Update 3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -24,28 +24,12 @@ def get_all_employees_data():
         else:
             print(f"Error: Unable to fetch data for employee ID {employee_id}")
 
-    # Check if all employee IDs are present in the output
-    all_employee_ids = set(range(1, 11))
-    existing_employee_ids = set(all_employees_data.keys())
-
-    if all_employee_ids == existing_employee_ids:
-        print("All users found: OK")
-    else:
-        missing_employee_ids = all_employee_ids - existing_employee_ids
-        print(f"Error: Missing data for employee IDs {missing_employee_ids}")
-
     # Export data to JSON
     json_filename = "todo_all_employees.json"
     with open(json_filename, 'w') as jsonfile:
         json.dump(all_employees_data, jsonfile, indent=4)
 
     print(f"Data exported to {json_filename} successfully.")
-
-    # Debugging output for each user ID
-    for employee_id, tasks in all_employees_data.items():
-        print(f"User ID: {employee_id}")
-        for task in tasks:
-            print(f"  Task: {task['task']}, Completed: {task['completed']}")
 
 if __name__ == "__main__":
     get_all_employees_data()
