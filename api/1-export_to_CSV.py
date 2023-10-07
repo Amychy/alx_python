266211bad6ce0ca55e397cb8ce5e00fd1e599d85
Amychy <119742258+Amychy@users.noreply.github.com>
2023-10-08 00:10:09 +0100

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -4,40 +4,40 @@ import requests
 import sys
 
 def user_info(employee_id):
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-
-    employee_response = requests.get(employee_url)
-    todos_response = requests.get(todos_url)
-
-    if employee_response.status_code != 200 or todos_response.status_code != 200:
-        print("Error: Unable to fetch data from the API.")
+    # Fetch employee information
+    employee_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    if employee_response.status_code != 200:
+        print("Error: Unable to fetch employee data.")
         return
 
     employee_data = employee_response.json()
-    todo_data = todos_response.json()
     employee_name = employee_data.get("name", "Unknown Employee")
+    employee_username = employee_data.get("username", "Unknown Username")
 
-    # Export data to CSV
+    # Fetch employee's tasks
+    todos_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    if todos_response.status_code != 200:
+        print(f"Error: Unable to fetch tasks for employee ID {employee_id}.")
+        return
+
+    todo_data = todos_response.json()
+
+    # Create a CSV file
     csv_filename = f"{employee_id}.csv"
+
     with open(csv_filename, mode="w", newline="") as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
         for task in todo_data:
             task_completed_status = "Completed" if task["completed"] else "Not Completed"
-            csv_writer.writerow([employee_id, employee_name, task_completed_status, task["title"]])
+            csv_writer.writerow([employee_id, employee_username, task_completed_status, task["title"]])
 
-    # Check if the CSV file exists before opening it
-    if os.path.exists(csv_filename):
-        with open(csv_filename, 'r') as f:
-            # Perform any required operations with the file
-            pass
-    else:
-        print(f"Error: CSV file '{csv_filename}' does not exist.")
+    print(f"CSV file '{csv_filename}' created successfully.")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        print("Usage: python3 main_0.py <employee_id>")
         sys.exit(1)
 
     try:
